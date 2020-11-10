9f7924971cfd8a78ea6dc08a471aeb4de8bee87e
Kushal Garg <kushalgarg2000@gmail.com>
2020-11-10 08:41:48 +0530

    Adds a level 1 question

---
+++ Level 1/Q1 (Kushal)/index.js
@@ -0,0 +1,2 @@
+"use strict";
+var _this = void 0;function _newArrowCheck(innerThis, boundThis) {if (innerThis !== boundThis) {throw new TypeError("Cannot instantiate an arrow function");}}var hash = function hash(text) {_newArrowCheck(this, _this);var enc = new TextEncoder();return enc.encode(text).join("");}.bind(void 0);
