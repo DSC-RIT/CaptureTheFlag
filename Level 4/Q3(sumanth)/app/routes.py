852735e99afcfc78aa47795969bcb1820b32a7e5
Sumanth108 <sumanth13900@gmail.com>
2020-11-23 17:22:37 +0530

    Minor changes

--- Level 4/Q3(sumanth)/app/routes.py
+++ Level 4/Q3(sumanth)/app/routes.py
@@ -2,15 +2,14 @@
 from app import app
 from flask import session,render_template
 
-
-
 app.secret_key="13092000"
-
-
 
 @app.route('/')
 def method_name():
    return "WRONG PAGE, GO TO /Question.html to see the question"
+
+
+
 @app.route('/home')
 def secret():
     return render_template('home.html')
